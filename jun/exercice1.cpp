71788bb68c7993efd00a1916ae93ca08e1899134
Juuunnne <jzhi@gaming.tech>
2022-01-24 16:06:31 +0100

    beginning E3

--- jun/exercice1.cpp
+++ jun/exercice1.cpp
@@ -2,25 +2,45 @@
 
 using namespace std;
 
+//Initialize variables  constructor//
 MainSDLWindow::MainSDLWindow(){
      window   = NULL;
      renderer = NULL;
      
 }
-
+//Destrutor//
 MainSDLWindow::~MainSDLWindow(){
     SDL_DestroyWindow( window );
+    SDL_DestroyRenderer(renderer);
     SDL_Quit();
 }
-
-int MainSDLWindow::Init(const char* title, int Window_height, int Window_weight){
-   SDL_Init(SDL_INIT_VIDEO);
-   window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Window_weight, Window_height, SDL_WINDOW_SHOWN );
+//Create window and renderer// 
+int MainSDLWindow::Init(const char* title, int Window_height, int Window_width){
+    SDL_Init(SDL_INIT_VIDEO);
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }   
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Window_width, Window_height, SDL_WINDOW_SHOWN |SDL_WINDOW_RESIZABLE );
+    if( window == 0 )
+    {
+        printf( "Window could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }    
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if( renderer == 0 )
+    {
+        printf( "Renderer could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    
+    
+    SDL_Rect rectangle = {0,0,100,100};
+    SDL_RenderDrawRect(renderer, const)
 
 }
 
 SDL_Renderer * MainSDLWindow::GetRenderer(void){
-
+    
+    return renderer; 
 }
 
 
--- jun/exercice1.hpp
+++ jun/exercice1.hpp
@@ -4,11 +4,23 @@ class MainSDLWindow{
 public:
      MainSDLWindow();
     ~MainSDLWindow();
+    
     int Init(const char* title, int Window_height, int Window_weight);
     SDL_Renderer * GetRenderer(void);
 
 private:
     SDL_Window *     window;
     SDL_Renderer * renderer;
+
+};
+
+class Movement{
+public:
+    Movement();
+    int moveForward;
+    int moveBack;
+    int moveLeft;
+    int moveRight;
+private:
 
 };
