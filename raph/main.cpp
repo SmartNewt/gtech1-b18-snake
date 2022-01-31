d3ec09f998d85065c74d9d9248124e07fc5acb56
raph <mateescuraphael@icloud.com>
2022-01-31 13:35:49 +0100

    debut cours lundi semaine 3

--- raph/main.cpp
+++ raph/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include "Game.hpp"
 
@@ -10,24 +9,4 @@ int main(int argc, char * argv[])
     Game().Run();
     cout << "Game has terminated successfully" << endl;
     return 0;
-=======
-#include <SDL2/SDL.h>
-#include "MainSDLWindow.hpp"
-
-int main(void){
-    MainSDLWindow main_window;
-    main_window.Init("Snake", 800, 800);
-    bool quit = false;
-    SDL_Event e ;
-    Uint32 frame_rate = 20;
-    while( !quit ){
-        //Handle events on queue
-        while( SDL_PollEvent( &e ) != 0 ){
-        //User requests quit
-            if( e.type == SDL_QUIT ){
-                quit = true;
-            }
-        }
-    }
->>>>>>> origin/main
 }
